e17eb354d54ca0d0f614dbb24affa3b9a6312c24
littleflute <13699175164@sina.cn>
2017-07-21 13:22:48 -0500

    Add files via upload

---
+++ Vitoria suite/dvd/m.js
@@ -0,0 +1,12 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "VitoriaSuite-_VTS_01_1 00_00_00-00_07_10.mp4";		
+f[2]	= "VitoriaSuite-_VTS_01_1 00_07_00-00_14_10.mp4";		
+f[3]	= "VitoriaSuite-_VTS_01_1 00_14_00-.mp4";		
+f[4]	= "VitoriaSuite-_VTS_01_2 00_00_00-00_08_10.mp4";		
+f[5]	= "VitoriaSuite-_VTS_01_2 00_08_00-00_16_10.mp4";		
+f[6]	= "VitoriaSuite-_VTS_01_2 00_16_00-.mp4";		
+f[7]	= "VitoriaSuite-_VTS_01_3.mp4";		
+f[8]	= "VitoriaSuite-_VTS_02_1 00_00_00-00_07_10.mp4";		
+f[9]	= "VitoriaSuite-_VTS_02_1 00_07_00-00_14_10.mp4";		
+f[10]	= "VitoriaSuite-_VTS_02_1 00_14_00-.mp4";		
